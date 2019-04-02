e11a6b9bc351563a1fa418fd3c50e02134dce20a
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-04-01 21:43:18 -0400

    simplify model formulation with ode_def macro

--- HPmodel.jl
+++ HPmodel.jl
@@ -1,16 +1,14 @@
-###Hastings & Powell 1991
-###Model equations
+### Hastings & Powell 1991
+### Model equations
 
-###Packages
-using Plots, DifferentialEquations
+### Packages
+using ParameterizedFunctions
 
-###Define the parameters and equations
+### Define parameters and equations
 f(u,a,b)=a*u/(1+b*u)
 
-function parameterized_model(du,u,p,t)
-    x,y,z = u
-    a1,a2,b1,b2,d1,d2 = p
-    du[1] = dx = x*(1-x) - f(x,a1,b1)*y
-    du[2] = dy = f(x,a1,b1)*y - f(y,a2,b2)*z - d1*y
-    du[3] = dz = f(y,a2,b2)*z - d2*z
-end
+parameterized_model = @ode_def begin
+    dx = x*(1-x) - f(x,a1,b1)*y
+    dy = f(x,a1,b1)*y - f(y,a2,b2)*z - d1*y
+    dz = f(y,a2,b2)*z - d2*z
+end a1 a2 b1 b2 d1 d2
--- figures4.jl
+++ figures4.jl
@@ -1,16 +1,16 @@
-###Hastings & Powell 1991
-##Reproducing figure 4
+### Hastings & Powell 1991
+## Reproducing figure 4
 
-###Figure 4 (Bifurcation diagram)
+### Figure 4 (Bifurcation diagram)
+
+# Load model
 include("HPmodel.jl")
-p = [5.0,0.1,2.0,2.0,0.4,0.01]
 
-#Packages
+# Packages
 using Plots, DifferentialEquations
 
-## Attempt 5 - Function within for loop with explicit parameter values
-# Previous attempts didn't work but were kept below
-using DifferentialEquations
+# Define initial parameters
+p = a1,a2,b1,b2,d1,d2 = [5.0,0.1,2.0,2.0,0.4,0.01]
 u0 = [1.0,1.0,1.0]
 tspan = (0.0,10000.0)
 bs = collect(2.0:0.01:3.2)
@@ -18,23 +18,18 @@ N = 42
 Z = zeros(Float64, (N,length(bs)))
 Zmax = zeros(Float64, length(bs))
 for j in 1:length(bs)
-  b1 = bs[j]
-  function model(du,u,p,t)
-    x,y,z = u
-    # a1,b1,a2,b2,d1,d2 = p # not working for now
-    du[1] = dx = x*(1 - x) - (5.0*x/(1 + b1*x))*y
-    du[2] = dy = (5.0*x/(1 + b1*x))*y - (0.1*y/(1 + 2.0*y))*z - 0.4*y
-    du[3] = dz = (0.1*y/(1 + 2.0*y))*z - 0.01*z
-  end
-  prob = ODEProblem(model,u0,tspan)
+  # Change b1 value
+  p[3] = b1 = bs[j]
+  # Solve for given b1 value
+  prob = ODEProblem(parameterized_model,u0,tspan,p)
   sol = solve(prob)
+  # Save values
   Z[:,j] = sol[3,end-(N-1):end]
   Zmax[j] = maximum(sol[3,:])
 end
 Z
+Zmax
 B = repeat(bs, inner=42)
 points = unique(hcat(B, vec(Z)); dims=1)
-using Plots
-scatter(points)
 plot(points[:,1], points[:,2])
 plot(Zmax)
