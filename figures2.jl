b6a700c7865032153dda318b5bf9afcbde2787c5
Elodie Basque <elodie.basque@umontreal.ca>
2019-04-02 12:24:38 -0400

    remove figure 3 to put it in a different file

--- figures2.jl
+++ figures2.jl
@@ -1,5 +1,5 @@
 ###Hastings & Powell 1991
-### Reproducing figures 2 and 3
+### Reproducing figures 2
 
 include("HPmodel.jl")
 
@@ -18,20 +18,3 @@ sol = solve(prob) #have to make sure which alg (RK4?) and reltol (1e-10?) are co
 plot(sol,vars=(0,1), xlim=(5000,6500), ylim=(0,1)) #2a
 plot(sol,vars=(0,2), xlim=(5000,6500), ylim=(0,0.5)) #2b
 plot(sol,vars=(0,3), xlim=(5000,6500), ylim=(7,11)) #2c
-
-###Figure 3,
-pl=plot(sol, vars=1, xlim=(0,2000), ylim=(0,1))
-
-##Attempt figure 3 changing b1 to 3.001
-#probably not the most efficient way to do it, find another solution
-#difference seen only on a larger timescale, have to find why
-function model(du,u,p,t)
-  x,y,z = u
-  du[1] = dx = x*(1 - x) - (5.0*x/(1 + 3.001*x))*y
-  du[2] = dy = (5.0*x/(1 + 3.001*x))*y - (0.1*y/(1 + 2.0*y))*z - 0.4*y
-  du[3] = dz = (0.1*y/(1 + 2.0*y))*z - 0.01*z
-end
-prob2 = ODEProblem(model,u0,tspan)
-sol2 = solve(prob)
-
-plot!(sol2,vars=1,xlim=(0,2000), ylim=(0,1))
