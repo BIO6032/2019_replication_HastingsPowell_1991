ea616b0e460ba8746535de14d26a0335b3b2e292
Elodie Basque <elodie.basque@umontreal.ca>
2019-04-04 11:49:12 -0400

    changing u0 and export figure 3

--- figure3.jl
+++ figure3.jl
@@ -7,7 +7,7 @@ include("HPmodel.jl")
 using Plots, DifferentialEquations
 
 #initial conditions serie 1
-u0=[0.75,0.15,9.75]   #initial conditions for x,y,z
+u0=[0.77,0.16,9.9]   #initial conditions for x,y,z
 tspan=(0.0,10000) #timespan
 p = [5.0,0.1,3.0,2.0,0.4,0.01]
 
@@ -17,11 +17,14 @@ sol = solve(prob)
 plot(sol, vars=1, xlim=(0,500), ylim=(0,1))
 
 #initial condition changing x by 0.01
-u0=[0.76,0.15,9.75]
+u0=[0.78,0.16,9.9]
 tspan=(0.0,10000) #timespan
 p = [5.0,0.1,3.0,2.0,0.4,0.01]
 
 prob = ODEProblem(parameterized_model,u0,tspan,p)
 sol = solve(prob)
 
-plot!(sol,vars=1,xlim=(0,500), ylim=(0,1), linestyle=:dash)
+fig3=plot!(sol,vars=1,xlim=(0,500), ylim=(0,1), linestyle=:dash)
+
+## Export figures
+savefig(fig3, "figures/fig3")
