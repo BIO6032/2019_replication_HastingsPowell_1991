96aceb12fd8526f04b092340843a5662b8ca5aaf
Elodie Basque <elodie.basque@umontreal.ca>
2019-04-11 11:15:31 -0400

    adding labels and removing legend

--- code/figure2.jl
+++ code/figure2.jl
@@ -15,9 +15,12 @@ prob = ODEProblem(parameterized_model,u0,tspan,p)
 sol = solve(prob) #have to make sure which alg (RK4?) and reltol (1e-10?) are correct
 
 ###Figure 2
-fig2a = plot(sol,vars=(0,1), xlim=(5000,6500), ylim=(0,1)) #2a
-fig2b = plot(sol,vars=(0,2), xlim=(5000,6500), ylim=(0,0.5)) #2b
-fig2c = plot(sol,vars=(0,3), xlim=(5000,6500), ylim=(7,11)) #2c
+fig2a = plot(sol,vars=(0,1), xlim=(5000,6500), ylim=(0,1),
+        xlabel="time", ylabel="x", linewidth=0.01, leg=false) #2a
+fig2b = plot(sol,vars=(0,2), xlim=(5000,6500), ylim=(0,0.5),
+        xlabel="time", ylabel="y",linewidth=0.01,leg=false) #2b
+fig2c = plot(sol,vars=(0,3), xlim=(5000,6500), ylim=(7,11),
+        xlabel="time", ylabel="z", linewidth=0.01,leg=false) #2c
 
 ## Export figures
 savefig(fig2a, "article/figures/fig2a")
