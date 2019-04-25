13b626823f5afc93027ab10e9e02b2d6af36d093
Elodie Basque <elodie.basque@umontreal.ca>
2019-04-25 09:36:21 -0400

    remove grids

--- code/figure2.jl
+++ code/figure2.jl
@@ -16,11 +16,14 @@ sol = solve(prob) #have to make sure which alg (RK4?) and reltol (1e-10?) are co
 
 ###Figure 2
 fig2a = plot(sol,vars=(0,1), xlim=(5000,6500), ylim=(0,1),
-        xlabel="time", ylabel="x", linewidth=0.01, leg=false) #2a
+        xlabel="time", ylabel="x", linewidth=0.01, leg=false,
+        grid=:none) #2a
 fig2b = plot(sol,vars=(0,2), xlim=(5000,6500), ylim=(0,0.5),
-        xlabel="time", ylabel="y",linewidth=0.01,leg=false) #2b
+        xlabel="time", ylabel="y",linewidth=0.01,leg=false,
+        grid=:none) #2b
 fig2c = plot(sol,vars=(0,3), xlim=(5000,6500), ylim=(7,11),
-        xlabel="time", ylabel="z", linewidth=0.01,leg=false) #2c
+        xlabel="time", ylabel="z", linewidth=0.01,leg=false,
+        grid=:none) #2c
 
 ## Export figures
 savefig(fig2a, "article/figures/fig2a")
