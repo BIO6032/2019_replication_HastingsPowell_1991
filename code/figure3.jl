4ad859a4cc7fcf567201273a65a8256aed3b924e
Elodie Basque <elodie.basque@umontreal.ca>
2019-04-25 09:42:30 -0400

    remove grids and legend

--- code/figure3.jl
+++ code/figure3.jl
@@ -24,7 +24,8 @@ p = [5.0,0.1,3.0,2.0,0.4,0.01]
 prob = ODEProblem(parameterized_model,u0,tspan,p)
 sol = solve(prob)
 
-fig3=plot!(sol,vars=1,xlim=(0,500), ylim=(0,1), linestyle=:dash)
+fig3=plot!(sol,vars=1,xlim=(0,500), ylim=(0,1), linestyle=:dash,
+        grid=:none, legend=false, xlabel="time", ylabel="x")
 
 ## Export figures
 savefig(fig3, "figures/fig3")
