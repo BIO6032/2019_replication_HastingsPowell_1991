fa1d15d4b4debc2347c23ec3ab09ee3a0227a7af
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-01-15 14:00:54 -0500

    add comment on working directory

--- code/main.jl
+++ code/main.jl
@@ -1,9 +1,17 @@
+# Scripts assume that `code/` is used as working directory
+# From project root, it can be set with
+# cd("./code/")
+# or in Atom: right-click code/ > Juno:Work in folder
+
+# Activate project environment
 import Pkg; Pkg.activate(".")
 
+# Load required packages
 using ParameterizedFunctions
 using Plots
 using DifferentialEquations
 
+# Load Hastings & Powell's model
 include("HPmodel.jl")
 
 # Make the figures
