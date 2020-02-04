e253f1a2641037aab87d6a5a44c9a79ba89fb346
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-02-04 15:17:19 -0500

    update figure 2D with flipped & pyplot options

--- code/figure2D.jl
+++ code/figure2D.jl
@@ -17,15 +17,43 @@ sol_y_b3 = sol_b3[2,:] # timeseries for y
 sol_z_b3 = sol_b3[3,:] # timeseries for z
 
 # Teacup Figures 2D and 5E (Three-dimentional phase plot)
-fig2d = plot(sol_b3,vars=(1,2,3),
+gr()
+fig2d = plot(sol_x_b3, sol_y_b3, sol_z_b3, #sol_b3,vars=(1,2,3),
              linewidth=0.01, linestyle=:dot,
              xlabel="x", xflip=false, xlim=(0.0,1.0), xticks=0.0:0.5:1.0,
              ylabel="y", yflip=true, ylim=(0.0,0.5), yticks=0.0:0.25:0.5,
              zlabel="z", zflip=false, zlim=(7.5,10.5), zticks=7.5:1.5:10.5,
-             grid=:none,
+             tick_direction=:out, guidefontsize=12, tickfontsize=12,
+             legend =:none, size=(600,600), c=:black,
+             right_margin=5Plots.mm, left_margin=2Plots.mm,
+             )
+# Seems like there's a bug with the axes labels when flipping y
+# Only solution we could find was to substract y values from 0.5, then plot and replace labels
+sol_y_b3_flip = copy(sol_y_b3)
+sol_y_b3_flip = 0.5 .- sol_y_b3_flip
+fig2d_flip = plot(sol_x_b3, sol_y_b3_flip, sol_z_b3, #sol_b3,vars=(1,2,3),
+             linewidth=0.01, linestyle=:dot,
+             xlabel="x", xflip=false, xlim=(0.0,1.0), xticks=0.0:0.5:1.0, xrotation=30.0,
+             ylabel="y", yflip=false, ylim=(0.0,0.5), yticks=(0.0:0.25:0.5, string.(0.5:-0.25:0.0)), yrotation=-10.0,
+             zlabel="z", zflip=false, zlim=(7.5,10.5), zticks=7.5:1.5:10.5, zrotation=0.0,
+             tick_direction=:out, guidefontsize=12, tickfontsize=12,
+             legend =:none, size=(600,600), c=:black,
+             right_margin=5Plots.mm, left_margin=2Plots.mm,
+             )
+# Otherwise, it's also possible to use PyPlot
+pyplot()
+fig2d_pyplot = plot(sol_x_b3, sol_y_b3, sol_z_b3, #sol_b3,vars=(1,2,3),
+             linewidth=0.1,
+             xlabel="x", xflip=false, xlim=(0.0,1.0), xticks=0.0:0.5:1.0,
+             ylabel="y", yflip=true, ylim=(0.0,0.5), yticks=0.0:0.25:0.5,
+             zlabel="z", zflip=false, zlim=(7.5,10.5), zticks=7.5:1.5:10.5,
+             tick_direction=:out, guidefontsize=12, tickfontsize=12,
              legend =:none, size=(600,600), c=:black)
-
-savefig(fig2d, joinpath("..", "article", "figures", "fig2d")) # export figure
+gr()
+# Export figures
+savefig(fig2d, joinpath("..", "article", "figures", "fig2d_misplaced"))
+savefig(fig2d_flip, joinpath("..", "article", "figures", "fig2d"))
+savefig(fig2d_pyplot, joinpath("..", "article", "figures", "fig2d_pyplot"))
 
 #### Teacup GIF
 ## Inspired by an example of Lorez attractor: http://docs.juliaplots.org/latest/
