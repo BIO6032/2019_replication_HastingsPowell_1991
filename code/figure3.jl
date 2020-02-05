320591cfadacb1740f4f64ae055b325f352baa07
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-02-05 10:03:05 -0500

    update figure 3 linewidth

--- code/figure3.jl
+++ code/figure3.jl
@@ -1,26 +1,24 @@
 ###Hastings & Powell 1991
 ### Reproducing figure 3
 
-#initial conditions serie 1
-u0=[0.77,0.16,9.9]   #initial conditions for x,y,z
-tspan=(0.0,10000) #timespan
-p = [5.0,0.1,3.0,2.0,0.4,0.01]
+tspan=(0.0,10000) # timespan
+p = [5.0,0.1,3.0,2.0,0.4,0.01] # parameter values
 
-prob = ODEProblem(parameterized_model,u0,tspan,p)
-sol = solve(prob)
+# Initial conditions serie 1
+u0_serie1=[0.77,0.16,9.9]   #initial conditions for x,y,z
+prob_serie1 = ODEProblem(parameterized_model,u0_serie1,tspan,p)
+sol_serie1 = solve(prob_serie1)
 
-plot(sol, vars=1, xlim=(0,500), ylim=(0,1), c=:black)
+plot(sol_serie1, vars=1, xlim=(0,500), ylim=(0,1), c=:black, linewidth=2)
 
-#initial condition changing x by 0.01
-u0=[0.78,0.16,9.9]
-tspan=(0.0,10000) #timespan
-p = [5.0,0.1,3.0,2.0,0.4,0.01]
+# Initial conditions changing x by 0.01
+u0_serie2=[0.78,0.16,9.9]
 
-prob = ODEProblem(parameterized_model,u0,tspan,p)
-sol = solve(prob)
+prob_serie2 = ODEProblem(parameterized_model,u0_serie2,tspan,p)
+sol_serie2 = solve(prob_serie2)
 
-fig3=plot!(sol,vars=1,xlim=(0,500), ylim=(0,1), linestyle=:dash,
-        grid=:none, legend=false, xlabel="time", ylabel="x", c=:darkgrey)
+fig3=plot!(sol_serie2,vars=1,xlim=(0,500), ylim=(0,1), linestyle=:dash, linewidth=2,
+        grid=:none, legend=false, xlabel="time", ylabel="x", c=:darkgrey, framestyle=:box)
 
 ## Export figures
 savefig(fig3, joinpath("..", "article", "figures", "fig3"))
